56f1d10c6442af9f6e0b18748b20423c0e2786b4
chmir <73581867+chmir@users.noreply.github.com>
2024-06-20 20:58:35 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 5668 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cs
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cs
@@ -1,33 +1,21 @@
-using System;
-using System.Collections.Generic;
 //bj2178 / 미로 탐색 / 240211
 class Program
 {
     //항상 이동할 수 있는 경우만 입력받기에 예외처리 안해도 되고
-    //음... 배열을 3개나 써야할까? 하나로 줄일 수는 없나?
+    //이 문제의 경우 disMap을 쓰지 않아도 될 거 같다. 함 해보자
     static int N, M;
     //맵
     static int[,] map = new int[101, 101]; //1~100 * 1~100
-    static bool[,] visited = new bool[101, 101]; //방문 확인
-    static int[,] disMap = new int[101, 101]; //시작점부터의 거리
+    static bool[,] visited = new bool[101, 101]; //방문 확인 
     //BFS용 큐 인접행렬 방식이니 x, y 좌표 저장
     static Queue<(int, int)> queue = new Queue<(int, int)>();
+    //상하좌우 탐색용 좌표배열
+    static readonly int[] dx = { -1, 1, 0, 0 };
+    static readonly int[] dy = { 0, 0, -1, 1 };
 
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
-
-    static void Reset() //방문배열 초기화
-    {
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-                visited[i, j] = false;
-            }
-        }
-    }
     static void BFS(int x, int y)
     {
+        //map[x, y] = 0; //첫 시작점의 거리를 1로할지 0으로 할지?
         queue.Enqueue((x, y)); //첫 시작점을 넣고
         visited[x, y] = true; //방문 완료
 
@@ -39,7 +27,6 @@ class Program
             int curx = cur.Item1;
             int cury = cur.Item2;
 
-
             //상하좌우 체크
             for (int i = 0; i < 4; i++)
             {
@@ -48,41 +35,41 @@ class Program
                 //인덱스 범위를 벗어나면 continue
                 if (curx < 0 || cury < 0 || curx > N || cury > M)
                     continue;
-                //이미 방문했거나 해당 집이 0이면 continue
+                //이미 방문했거나 벽이라면 continue
                 if (visited[curx, cury] == true || map[curx, cury] == 0)
                     continue;
+                //방문하지 않은 길이라면 방문 표시를 남기고
                 visited[curx, cury] = true;
+                //다음에 탐색하도록 큐에 넣어준다. 
+                //큐는 가장 이전에 입력받은 게 먼저 출력되므로 
+                //탐색방식도 DFS와 달리 넓게넓게 퍼지면서 탐색할 수 있게 된다.
                 queue.Enqueue((curx, cury));
 
-                //아까 Pop했던 정보를 기억해두고 거리 맵 배열에 += 해줌
-                disMap[curx, cury] += disMap[cur.Item1, cur.Item2];
+                //지금 탐색한 위치는 이전 위치의 거리값에 +1한 값이다
+                map[curx, cury] = map[cur.Item1, cur.Item2] + 1;
             }
         }
 
     }
     static void Main()
     {
-        //N, M
+        //N, M 입력
         int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
         N = arr[0];
         M = arr[1];
 
-        //map배열 초기화
+        //map 입력
         for (int i = 0; i < N; i++)
         {
             string input = Console.ReadLine();
-            for (int j = 0; j < input.Length; j++)
-            {
-                map[i, j] = Convert.ToInt32((input[j].ToString()));
-                disMap[i, j] = Convert.ToInt32((input[j].ToString()));
-            }
+            for (int j = 0; j < M; j++) map[i, j] = input[j] - '0';
         }
 
-        //방문 배열 초기화
-        Reset();
+        //방문 배열은 어차피 테스트 케이스 하나만 쓰니까 초기화 필요없음
+
         //시작
         BFS(0, 0);
-
-        Console.WriteLine(disMap[N - 1, M - 1]);
+        //정답
+        Console.WriteLine(map[N - 1, M - 1]);
     }
 }
