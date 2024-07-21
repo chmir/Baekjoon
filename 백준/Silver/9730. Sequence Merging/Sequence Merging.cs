f328fe1581b4582d50c5602ae0e588178e75c228
chmir <73581867+chmir@users.noreply.github.com>
2024-07-22 04:21:46 +0900

    [Silver I] Title: Sequence Merging, Time: 100 ms, Memory: 15168 KB -BaekjoonHub

--- 백준/Silver/9730. Sequence Merging/Sequence Merging.cs
+++ 백준/Silver/9730. Sequence Merging/Sequence Merging.cs
@@ -1,6 +1,5 @@
 //bj9730 /s1 /Sequence Merging /240722
-//주어진 수열을 하나의 원소로 줄이기 위한 최소비용을 구해야함
-
+//stack, greedy
 class Program
 {
     static void Main(string[] args)
@@ -25,37 +24,47 @@ class Program
     // 최소 비용 계산 함수
     static int CalculateMinCost(int[] sequence)
     {
+        // 스택을 사용하여 수열의 원소를 저장
         Stack<int> stack = new Stack<int>();
-        int cost = 0; //최소비용
+        // 최소 비용을 저장할 변수
+        int cost = 0;
 
         // 첫 번째 원소를 스택에 추가
         stack.Push(sequence[0]);
-        // 연산 시작
+
+        // 두 번째 원소부터 수열의 끝까지 처리
         for (int i = 1; i < sequence.Length; i++)
         {
-            // 현재 원소를 스택의 top과 비교하여 더 큰 값을 top으로 설정
+            // 스택이 비어있지 않고, 현재 원소보다 다음에 넣을 원소가 크거나 같다면...
             while (stack.Count > 0 && stack.Peek() <= sequence[i])
             {
-                int top = stack.Pop(); 
+                // 스택의 top 원소를 꺼냄 (결과를 사용하지 않으므로 버림)
+                stack.Pop();
+                // 스택이 비어있지 않다면, 스택의 새 top 원소와 현재 원소 중 작은 값을 비용에 더함
                 if (stack.Count > 0)
                 {
                     cost += Math.Min(stack.Peek(), sequence[i]);
                 }
+                // 스택이 비어있다면, 현재 원소를 비용에 더함
                 else
                 {
                     cost += sequence[i];
                 }
             }
+            // 현재 원소를 스택에 추가
             stack.Push(sequence[i]);
         }
 
-        // 스택에 남은 원소들에 대해 비용 계산
+        // 스택에 남아 있는 원소들에 대해 비용을 계산
         while (stack.Count > 1)
         {
-            int top = stack.Pop();
+            // 스택의 top 원소를 꺼냄 (결과를 사용하지 않으므로 버림)
+            stack.Pop();
+            // 남아 있는 스택의 원소를 비용에 더함
             cost += stack.Peek();
         }
 
+        // 총 비용을 반환
         return cost;
     }
 }
