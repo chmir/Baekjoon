5086ce9e65f246c6fad9ad9826e765df81b58cae
chmir <73581867+chmir@users.noreply.github.com>
2024-10-11 22:12:24 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 112 ms, Memory: 20832 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cs
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cs
@@ -1,48 +1,60 @@
-using System;
-using System.Collections.Generic;
+//bj27172 /g4 /수 나누기 게임 /241011
+// 각 플레이어는 숫자가 적힌 카드를 가지고 있으며, 서로 배수를 기준으로 결투를 벌인다.
+// 각 플레이어의 카드 번호가 주어졌을 때, 배수 관계를 통해 이길 수 있는 플레이어를 찾고 점수를 계산함
+// 주어진 카드 번호를 기준으로 해당 카드의 배수에 다른 플레이어의 카드가 있는지 확인한 후, 승패를 결정한다.
+// 배수는 어디까지 확인하냐면 주어진 카드 중 가장 큰 카드 이상은 비교할 카드가 없기에 배수가 넘어가면 반복 종료
+// 이 문제는 풀이 방식이 쉬워서 편한듯, 입력받는 수가 좀 많으니 빠른 입출력 코드 사용하자.
+using System.Text;
 
-class Program
+class Macross_82_99__Now_And_Forever
 {
-    static void Main()
+    static void Main(string[] args)
     {
+        //빠른 입력과 출력을 위한 StreamReader와 StreamWriter 설정
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        StringBuilder sb = new StringBuilder();
+
         // 플레이어 수 입력
-        int N = int.Parse(Console.ReadLine());
+        int N = int.Parse(sr.ReadLine());
 
-        List<int> numbers = new List<int>();
-        int[] scores = new int[1000006];
-        int[] cards = new int[1000006];
+        // 공백으로 구분된 카드 번호를 배열로 변환
+        int[] card = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
 
-        // 두 번째 줄에서 한 줄에 공백으로 구분된 카드 넘버 입력
-        string[] input = Console.ReadLine().Split();
+        // 가장 큰 카드 번호 찾기
+        int maxCard = card.Max(); //정수 배열은 Max메서드로 
 
+        // 카드 존재 여부를 저장하는 배열
+        bool[] dict = new bool[maxCard + 1];
         for (int i = 0; i < N; i++)
-        {
-            int temp = int.Parse(input[i]);
+            dict[card[i]] = true;
 
-            numbers.Add(temp); // 각 플레이어의 카드 넘버 저장
-            cards[temp] = 1; // 특정 플레이어가 해당 카드 넘버를 가지고 있는지 체크
-        }
+        // 각 카드 번호별 점수를 저장할 배열
+        int[] score = new int[maxCard + 1];
 
-        // 에라토스테네스의 체 응용
-        // 각 플레이어의 카드 넘버의 배수를 각각 체크하면서 해당 카드가 있다면, 결투
-        for (int i = 0; i < N; i++)
+        // 결투 로직: 배수를 찾고 점수 계산
+        for (int i = 0; i < N; i++) //모든 플레이이어가 한번씩 돌아가며...
         {
-            // 해당 카드 넘버의 배수마다 탐색
-            for (int j = numbers[i] * 2; j < 1000001; j += numbers[i])
+            int num = card[i];
+
+            // num의 배수에 대해 탐색
+            for (int j = 2; num * j <= maxCard; j++) //num의 2배 부터, 가장 큰 카드까지 하나씩 배수를 계산
             {
-                // 해당 카드 넘버의 배수의 카드가 존재하면 결투
-                if (cards[j] == 1)
+                if (dict[num * j]) //num으로 나눠지는 다른 플레이어의 카드가 있다면?
                 {
-                    scores[numbers[i]]++;
-                    scores[j]--;
+                    score[num]++; //승리한 플레이어
+                    score[num * j]--; //패배한 플레이어
                 }
             }
         }
 
-        // 각 플레이어의 점수 출력
+        // 각 플레이어의 점수를 StringBuilder에 저장
         for (int i = 0; i < N; i++)
-        {
-            Console.Write(scores[numbers[i]] + " ");
-        }
+            sb.Append(score[card[i]] + " ");
+
+        // 출력
+        sw.Write(sb.ToString().Trim());
+        sw.Close();
+        sr.Close();
     }
 }
